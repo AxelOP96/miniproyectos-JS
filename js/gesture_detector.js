5e3a9b8fa75ddec0dd2f25f131b7b7413a448440
Axel <axeelleguero@gmail.com>
2025-03-24 20:19:28 -0300

    Open camera

--- js/gesture_detector.js
+++ js/gesture_detector.js
@@ -1,8 +1,9 @@
 const d = document,
 n = navigator,
 w = window;
-//.n.mediaDevices, getUserMedia
+
 const $info = d.querySelector(".info");
+const camara = d.querySelector("#camara")
 export function showData(){
     $info.innerHTML = `Tu dispositivo es: ${n.userAgent} <br>
     Tu idioma predeterminado es ${n.language}`
@@ -11,9 +12,18 @@ export function showData(){
         console.log("Geolocalización no soportada")
     }
     
-
     function success(position){
         $info.insertAdjacentHTML("beforebegin", `Latitud:  ${position.coords.latitude}
     <br>Longitud:  ${position.coords.longitude} <br>Precisión:  ${position.coords.accuracy}`);
     }
+}
+export function cameraOn(){
+    n.mediaDevices.getUserMedia({
+        audio: false,
+        video: { width: 840, height: 600 },
+    }).then(function (media){
+        camara.srcObject = media;
+        camara.play();
+    })
+    
 }
