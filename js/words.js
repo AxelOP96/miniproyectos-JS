1b3eb81f6f104d8f281da7869dd7cf65ef85c4ba
Axel <axeelleguero@gmail.com>
2025-04-04 16:49:15 -0300

    FEAT(words.js) functionality that sustract tries

--- js/words.js
+++ js/words.js
@@ -1,5 +1,6 @@
 const $words = d.getElementById("words");
 const ALPHABET = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+const $try = d.getElementById("try");
 const palabras = [
     "aventuras", "alquileres", "francisco", "misterios", "recibidos", 
     "solitarios", "entusiasmo", "delicioso", "entrenado", "confianza", 
@@ -12,7 +13,7 @@ const palabras = [
     "transitar", "resolución", "despacho", "fugaz", "impulso", 
     "progreso", "necesario", "espectacular", "voluntario"
   ];
-  
+let intentos = 5;
 d.addEventListener("DOMContentLoaded", (e)=>{
 
     for(let i=0; i<ALPHABET.length; i++){
@@ -23,21 +24,56 @@ d.addEventListener("DOMContentLoaded", (e)=>{
     }
 
     const random = mostrarPalabra();
-    convertir(random);
+    const $letters = d.querySelectorAll(".letter");
+    probarLetra($letters, random);
+    
 })
 
 function mostrarPalabra(){
     const h2 = d.createElement("h2");
     h2.innerHTML = `${palabras[Math.floor(Math.random()*palabras.length)]}`;
     h2.classList.add("center", "word");
+    h2.setAttribute("id", "word");
     $words.insertAdjacentElement("beforebegin", h2)
     return h2;
 }
-function convertir(random){
-    console.log(random)
-    const newWord = "";
-    for(let i=0; i<random.length; i++){
-        newWord += "- "
+//recibo la palabra, reemplazo (replace) la palabra por un guion por cada letra, luego al dar click si esa letra coincide reemplazarla
+    //los dos strings la palabra elegida y la de guiones deberian estar en la misma funcion
+function probarLetra($letters, random){
+    console.log("random" , random.innerText)
+    const palabraElegida = random.innerText;
+    let newWord = "";
+    for(let i=0; i<random.innerText.length; i++){
+        newWord += "-"
     }
     random.innerHTML = `${newWord}`;
+    $letters.forEach((letter) =>{
+        letter.addEventListener("click", (e) => intentarLetra(e, random, palabraElegida));
+    })
+    function intentarLetra(e, palabra, palabraElegida){
+        //console.log(e.target)
+        //console.log(e.target.innerHTML)
+        if(palabraElegida.includes(e.target.innerHTML)){
+            //random.innerHTML = palabraElegida[j];
+            console.log("Está")
+            for(let i=0; i< palabraElegida.length; i++){
+                if(palabraElegida[i] === e.target.innerHTML){
+                    console.log("La letra a reemplazar es " , palabra.innerHTML[i] )
+                    let palabraArray = palabra.innerHTML.split("");
+                    palabraArray[i] = palabra.innerHTML[i];     // ["h", "e", "l", "a"]
+                    // Unimos el array nuevamente en un string
+                    palabra.innerHTML = palabraArray.join("");
+                    //palabra.innerHTML[i] = e.target.innerHTML;
+                    console.log(palabra.innerHTML[i])
+                }
+            }
+        }
+        else{
+            if(intentos > 0){
+                intentos--;
+                $try.setAttribute("src", `./assets/intentos-${intentos}.png`);
+            }
+            
+        }
+    }
 }
