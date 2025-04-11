d2e7cff7feea141286627c0be65a3174145fe260
Axel <axeelleguero@gmail.com>
2025-04-11 00:12:51 -0300

    FEAT(CSS) spans styled and priorities added

--- js/priority_list.js
+++ js/priority_list.js
@@ -1,19 +1,28 @@
 const $priorityBox = d.getElementById("priority"),
 $priorityList = d.querySelector(".priority-list");
 crearPrioridades();
+let count = 1;
 d.addEventListener("keyup", (e)=>{
 
-    if(e.target === $priorityBox && e.key === 'Enter') 
+    if(e.target === $priorityBox && e.key === 'Enter') {
+        
+        desbloquearTarea(count, e);
+        count ++;
+    }
 
-    console.log("True")
     
+    function desbloquearTarea(count, e){
+        const spanDesbloqueado = d.querySelector(`#tarea${count}`);
+        spanDesbloqueado.classList.remove("none");
+        spanDesbloqueado.innerHTML = e.target.value;
+    }
     
 })
 function crearPrioridades(){
     for(let i=0; i<20; i++){
         const span = d.createElement("span");
-        span.setAttribute("id", `${i}`);
-        //span.classList.add("none", "task");
+        span.setAttribute("id", `tarea${i+1}`);
+        span.classList.add( "task");//"none",
         span.innerHTML = `Tarea ${i+1}`;
         $priorityList.appendChild(span)
     }
