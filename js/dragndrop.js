cb8e962103090c2567901c5b7b8f2c07bdd23195
Axel <axeelleguero@gmail.com>
2025-04-17 23:02:00 -0300

    FEAT(js) API bring image and name

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,9 +1,18 @@
 const CARDS = 10;
 
-fetch("https://pokeapi.co/api/v2/pokemon/")
-.then(response => response.json())
-.then(data => mostrarPokemon(data));
+fetch("https://pokeapi.co/api/v2/pokemon/25")
+    .then(response => response.json())
+    .then(data => mostrarPokemon(data));
 
 function mostrarPokemon(data){
     console.log(data)
+    
+    const $drag = d.querySelector(".drag");
+    const $drop = d.querySelector(".drop");
+    const div = d.createElement("div");
+    const img = d.createElement("img");
+    img.classList.add("pokemon");
+    img.setAttribute("src",`${data.sprites.front_default}`);
+    $drop.innerHTML = `${data.name}`;
+    $drag.appendChild(img);
 }
