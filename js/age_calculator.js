b76558992d745fb3d84b7982e5190820810ca2f2
Axel <axeelleguero@gmail.com>
2025-04-18 22:26:24 -0300

    FEAT(js) delete comments

--- js/age_calculator.js
+++ js/age_calculator.js
@@ -10,12 +10,11 @@ iniciarCalculoEdad();
 
 d.addEventListener("change", (e)=>{
     let actual = new Date();
-    console.log("El dia es", actual.getDay())
+    //console.log("El dia es", actual.getDay())
     
     if(e.target === $inputYear) $years.innerHTML = `${actual.getFullYear() - $inputYear.value} YEARS`; 
     if(e.target === $inputMonth) $months.innerHTML = `${actual.getMonth()+1 - $inputMonth.value} MONTHS`; 
     if(e.target === $inputDay) $days.innerHTML = `${actual.getDay()-1 - $inputDay.value} DAYS`; 
-    
 })
 function iniciarCalculoEdad(){
     if ($inputYear.value == "") $years.innerHTML = `-- YEARS`;
